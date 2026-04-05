16fd2d1580ca54829a1f745e66e513258ea33ba5
Uselesslyfun <disscordboiiilmaoololtoxic@gmail.com>
2026-04-05 23:07:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let time = 0;
 
+function updateDisplay() {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    document.getElementById("timer").innerText =
+        `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
+
+function startStopwatch() {
+    updateDisplay(); // sets 00:00 instantly
+
+    setInterval(() => {
+        time++;
+        updateDisplay();
+    }, 1000);
+}
+
+window.onload = startStopwatch;
