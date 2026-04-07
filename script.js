2c9a48678c1b3214d686515c40bc574a974c270c
Uselesslyfun <disscordboiiilmaoololtoxic@gmail.com>
2026-04-07 23:36:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,9 @@ window.onload = function () {
         }
     });
 };
+const glow = document.getElementById("cursorGlow");
+
+document.addEventListener("mousemove", (e) => {
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
